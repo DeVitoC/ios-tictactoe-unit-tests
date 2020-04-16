fdae84395dfd10f9dc517140cd6cc20d5c8ea14a
Christopher DeVito <toolofjesus@protonmail.com>
2020-04-16 02:24:15 -0400

    implemented makeMark(at coordinate) in Game.swift

--- TicTacToeTests/GameTests.swift
+++ TicTacToeTests/GameTests.swift
@@ -6,10 +6,26 @@
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
-import Foundation
+import XCTest
 @testable import TicTacToe
 
-class GameTests {
+class GameTests: XCTestCase {
     
+    func testRestartGame() {
+        var game = Game()
+        var board = GameBoard()
+        
+        // place marks ending with o's turn next
+        try! board.place(mark: .x, on: (0, 0))
+        try! board.place(mark: .o, on: (1, 0))
+        try! board.place(mark: .x, on: (0, 1))
+        
+        // attempt to restart game - should be empty board and x's turn
+        board = game.restart()
+        
+        XCTAssertTrue(game.activePlayer == .x)
+        XCTAssertNoThrow(try! board.place(mark: .x, on: (0, 0)))
+        
+    }
     
 }
